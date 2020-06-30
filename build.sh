cafa6ae4835cbaaed9245a9dafd8b807e6c4bdcc
Jad Baz <jadbaz@gmail.com>
2020-06-30 09:08:15 +0000

    Fixed build

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 chmod +x gradlew
 ./gradlew build
+wd=`pwd`
 cd ./build/distributions
 rm -rf prometheus-java-example
 mkdir prometheus-java-example
-tar -xvf -C prometheus-java-example prometheus-java-example-1.0.tar --strip-components 1
+tar -xvf prometheus-java-example-1.0.tar -C prometheus-java-example --strip-components 1
+cd $wd
+docker build -t prometheus-java-example .
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run prometheus-java-example
